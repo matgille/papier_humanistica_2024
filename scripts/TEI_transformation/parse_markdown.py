5139207ebfcb24e6790bb57e9c05150fd8c77b53
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-02-12 20:18:44 +0100

    Comment script

--- scripts/TEI_transformation/parse_markdown.py
+++ scripts/TEI_transformation/parse_markdown.py
@@ -87,29 +87,25 @@ def xmlify(path):
     
     
     # GFM extension of marko parses tables too.
-    transformed = gfm.convert(transformed)
-    
-    document = transformed
-    
-    
+    converted_doc = gfm.convert(transformed)
     parser = ET.HTMLParser(recover=True)
-    parsed_doc = ET.fromstring(document, parser=parser)
     
     
     
     # We then Structure the document
+    lesson_as_xml_tree = ET.fromstring(converted_doc, parser=parser)
     
     ## First we need to correct the structure. The first level of division will be 1.
     all_headings = []
     for section_level in range(6):
-        all_headings.extend([element.tag.replace("h", "") for element in parsed_doc.xpath(f"//h{section_level}")])
+        all_headings.extend([element.tag.replace("h", "") for element in lesson_as_xml_tree.xpath(f"//h{section_level}")])
     different_headings = [int(val) for val in list(set(all_headings))]
     minimal_heading = min(different_headings)
     if minimal_heading != 1:
         difference = minimal_heading - 1
         print(difference)
         for section_level in range(minimal_heading, 7):
-            all_nodes = parsed_doc.xpath(f"descendant::h{section_level}")
+            all_nodes = lesson_as_xml_tree.xpath(f"descendant::h{section_level}")
             for heading in all_nodes:
                 heading.tag = f"h{str(section_level - difference)}"
     ## First we need to correct the structure. The first level of division will be 1.
@@ -120,15 +116,16 @@ def xmlify(path):
     for section_level in range(0, 6):
         level = section_level + 1
         if level == 1:
-            parent_nodes = [parsed_doc]
+            parent_nodes = [lesson_as_xml_tree]
         else:
-            # We use the tree updated in a previous run (h1 > div @n=1, then h2 > div @n=2, etc  
-            parent_nodes = parsed_doc.xpath(f"//div[@n = '{level - 1}']")
+            # We use the tree updated in a previous run (h1 > div @n=1, then h2 > div @n=2, etc)  
+            parent_nodes = lesson_as_xml_tree.xpath(f"//div[@n = '{level - 1}']")
         for parent_node in parent_nodes:
             first_node = parent_node.xpath(f"descendant::h{level}[1]")
             all_same_level_nodes = parent_node.xpath(f"descendant::h{level}[1]/following-sibling::node()")
             filtered_same_level_nodes = first_node + [element for element in all_same_level_nodes if isinstance(element, ET._Element)]
             clustered_nodes = {}
+            # This dict method is really usefull, unlike this comment
             for index, node in enumerate(filtered_same_level_nodes):
                 if node.tag == f"h{level}":
                     current_heading = node
@@ -136,21 +133,27 @@ def xmlify(path):
                 else:
                     clustered_nodes[current_heading].append(node)
             for heading, nodes in clustered_nodes.items():
-                parent = heading.getparent()
+                # Let's replace h{X} with <head/>
                 heading_level = heading.tag.replace("h", "")
                 heading.tag = "head"
-                element_and_childs = ET.Element(f"div")
-                element_and_childs.set("n", heading_level)
-                element_and_childs.append(heading)
+                # Add the division heading
+                division_and_descendants = ET.Element(f"div")
+                # Attribute n='X'
+                division_and_descendants.set("n", heading_level)
+                # Append the childs found with the dict method, starting with the new heading
+                division_and_descendants.append(heading)
                 for node in nodes:
-                    element_and_childs.append(node)
-                parent.append(element_and_childs)
+                    division_and_descendants.append(node)
+                # Find and append the newly division to its parent
+                parent = heading.getparent()
+                # We work sequentially, so we can append the division at the end of its parent.
+                parent.append(division_and_descendants)
     
     
     
     
     # TODO: move block codes in the previous paragraph (doesnt work, see in the future)
-    # for index, block_code in enumerate(parsed_doc.xpath("//pre/code", namespaces=ns_decl)):
+    # for index, block_code in enumerate(lesson_as_xml_tree.xpath("//pre/code", namespaces=ns_decl)):
     #     try:
     #         previous_p = block_code.xpath("parent::pre/preceding-sibling::p[1]")[0]
     #     except IndexError:
@@ -160,13 +163,15 @@ def xmlify(path):
     #     nodes_number = int(previous_p.xpath("count(child::node())"))
     #     previous_p.insert(nodes_number, block_code)
     # # Now remove the empty pre
-    # for empty_pre in parsed_doc.xpath("//pre", namespaces=ns_decl):
+    # for empty_pre in lesson_as_xml_tree.xpath("//pre", namespaces=ns_decl):
     #     empty_pre.getparent().remove(empty_pre)
         
     
     # Let's TEIfy it
-    root = parsed_doc.xpath("/node()")[0]
+    root = lesson_as_xml_tree.xpath("/node()")[0]
     # Converting HTML root to TEI, adding correct namespace
+    # TODO: this doesnt affect the whole tree, maybe xslt ? 
+    # see https://stackoverflow.com/a/51660868
     root.tag = "TEI"
     root.set('xmlns', tei_ns)
     root.insert(0, ET.fromstring(metadata_as_xml))
@@ -175,7 +180,7 @@ def xmlify(path):
     text_element.append(body[0])
     root.insert(1, text_element)
 
-    original_file = parsed_doc.xpath("//original")
+    original_file = lesson_as_xml_tree.xpath("//original")
     if len(original_file) == 1:
         dir_name = original_file[0].text
     else:
@@ -187,7 +192,7 @@ def xmlify(path):
 
     # Block code management: extract them from the document, and write them 
     # to a text file.
-    for index, block_code in enumerate(parsed_doc.xpath("//pre/code")):
+    for index, block_code in enumerate(lesson_as_xml_tree.xpath("//pre/code")):
         id = f"code_{lesson_name}_{index}"
         content = block_code.text
         block_code.text = ""
@@ -211,7 +216,7 @@ def xmlify(path):
         # TODO: happens with other chars. It's annoying.
         # TODO: Another problem with regexp lesson: last urls disapear.
         # TODO: linebreaks in code are removed by lxml. 
-        output_file.write(ET.tostring(parsed_doc, pretty_print=True).decode('utf-8'))
+        output_file.write(ET.tostring(lesson_as_xml_tree, pretty_print=True).decode('utf-8'))
 
         
 if __name__ == '__main__':
