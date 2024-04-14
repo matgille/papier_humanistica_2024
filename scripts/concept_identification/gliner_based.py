1dcb663607d1d6c35b6795cc600363a2f7d1775c
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-04-14 11:25:09 +0200

    Update gliner script

--- scripts/concept_identification/gliner_based.py
+++ scripts/concept_identification/gliner_based.py
@@ -1,24 +1,23 @@
 import tqdm
 from gliner import GLiNER
 import glob
-
+import sys
 model = GLiNER.from_pretrained("urchade/gliner_base")
 
 labels = ["technical term"]
-
-for lang in ['fr', 'es', 'pt', 'en']:
-    files = glob.glob(f"/home/mgl/Bureau/Travail/PH/jekyll/{lang}/*/*.md")
-    concat_lessons = ""
-    entities = []
-    for file in tqdm.tqdm(files):
-        with open(file, "r") as indiv_file:
-            lesson = indiv_file.read()
-        try:
-            identified_ents = model.predict_entities(lesson, labels, threshold=0.5)
-        except  IndexError:
-            continue
-        for entity in identified_ents:
-            entities.append(entity["text"])
+# f"/home/mgl/Bureau/Travail/PH/jekyll/{lang}/*/*.md"
+files = glob.glob(sys.argv[1])
+concat_lessons = ""
+entities = []
+for file in tqdm.tqdm(files):
+    with open(file, "r") as indiv_file:
+        lesson = indiv_file.read()
+    try:
+        identified_ents = model.predict_entities(lesson, labels, threshold=0.5)
+    except  IndexError:
+        continue
+    for entity in identified_ents:
+        entities.append(entity["text"])
 
     print(list(set(entities)))
     print(len(list(set(entities))))
