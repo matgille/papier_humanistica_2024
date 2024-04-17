dbfad302d188cd0abb0add0a9fb4b33835c211fe
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-04-17 12:42:56 +0200

    Start working on the microtextual alignment

--- scripts/microtextual_collation/microtextual_collation.py
+++ scripts/microtextual_collation/microtextual_collation.py
@@ -1,55 +1,98 @@
-import re
-import numpy as np
-import fasttext.util
-from huggingface_hub import hf_hub_download
-# from sentence_transformers import SentenceTransformer
-# model = SentenceTransformer("all-MiniLM-L6-v2")
+import json
+
+import torch
+import transformers
+import itertools
+import lxml.etree as ET
+model = transformers.BertModel.from_pretrained('Geotrend/bert-base-10lang-cased')
+tokenizer = transformers.BertTokenizer.from_pretrained('Geotrend/bert-base-10lang-cased')
 
 
+def run_align(src, tgt):
+    # pre-processing
+    sent_src, sent_tgt = src.strip().split(), tgt.strip().split()
+    token_src, token_tgt = [tokenizer.tokenize(word) for word in sent_src], [tokenizer.tokenize(word) for word in
+                                                                             sent_tgt]
+    wid_src, wid_tgt = [tokenizer.convert_tokens_to_ids(x) for x in token_src], [tokenizer.convert_tokens_to_ids(x) for
+                                                                                 x in token_tgt]
+    ids_src, ids_tgt = tokenizer.prepare_for_model(list(itertools.chain(*wid_src)), return_tensors='pt',
+                                                   model_max_length=tokenizer.model_max_length, truncation=True)[
+                           'input_ids'], \
+                       tokenizer.prepare_for_model(list(itertools.chain(*wid_tgt)), return_tensors='pt',
+                                                   truncation=True, model_max_length=tokenizer.model_max_length)[
+                           'input_ids']
+    sub2word_map_src = []
+    for i, word_list in enumerate(token_src):
+        sub2word_map_src += [i for x in word_list]
+    sub2word_map_tgt = []
+    for i, word_list in enumerate(token_tgt):
+        sub2word_map_tgt += [i for x in word_list]
 
-def cosine_similarity(expression_1, expression_2):
-    return np.dot(expression_1, expression_2) / (np.linalg.norm(expression_1) * np.linalg.norm(expression_2))
+    # alignment
+    align_layer = 8
+    threshold = .00001
+    model.eval()
+    with torch.no_grad():
+        out_src = model(ids_src.unsqueeze(0), output_hidden_states=True)[2][align_layer][0, 1:-1]
+        out_tgt = model(ids_tgt.unsqueeze(0), output_hidden_states=True)[2][align_layer][0, 1:-1]
 
+        dot_prod = torch.matmul(out_src, out_tgt.transpose(-1, -2))
 
-def embed():
-    pass
+        softmax_srctgt = torch.nn.Softmax(dim=-1)(dot_prod)
+        softmax_tgtsrc = torch.nn.Softmax(dim=-2)(dot_prod)
 
-def collate(source_sent:list, target_sent:list, source_spans, max_MWE_length:int) -> list:
-    """
-    This function allows to collate two sentences, and to identify in the target sentence a mono or multiword expression
-    :return: a list with the corresponding indices of the concepts 
-    source_sent: the tokenized source sentence, as a list of words
-    target_sent: the tokenized target sentence, as a list of words 
-    source_spans: a list of tuple containing the index of the word[s] to find in the target sentence
-    max_MWE_length: the maximum target length for the expression
-    """
+        softmax_inter = (softmax_srctgt > threshold) * (softmax_tgtsrc > threshold)
+
+    align_subwords = torch.nonzero(softmax_inter, as_tuple=False)
+    align_words = set()
+    for i, j in align_subwords:
+        align_words.add((sub2word_map_src[i], sub2word_map_tgt[j]))
+
+    # printing
+    class color:
+        PURPLE = '\033[95m'
+        CYAN = '\033[96m'
+        DARKCYAN = '\033[36m'
+        BLUE = '\033[94m'
+        GREEN = '\033[92m'
+        YELLOW = '\033[93m'
+        RED = '\033[91m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+        END = '\033[0m'
+
+    for i, j in sorted(align_words):
+        print(f'{color.BOLD}{color.BLUE}{sent_src[i]}{color.END}==={color.BOLD}{color.RED}{sent_tgt[j]}{color.END}')
+
+def main():
+
+    main_file = "../../data/tei_sentences_tokenized/main.xml"
+    as_tree = ET.parse(main_file)
+    as_tree.xinclude()
+    for sent in as_tree.xpath(f"descendant::tei:TEI[@type='original']/descendant::tei:s", namespaces=namespaces)[:10]:
+        all_texts = []
+        corresp_sent_id = sent.xpath("@corresp")[0]
+        corresp_dict = json.loads(corresp_sent_id.replace("'", "\""))
+        print(corresp_dict)
+        print(list(corresp_dict.values())[0][0])
+        corresponding_sentence = as_tree.xpath(f"descendant::tei:s[@xml:id='{list(corresp_dict.values())[0][0]}']", namespaces=namespaces)[0]
+        all_texts.append(text_from_tokens(sent))
+        as_text = text_from_tokens(corresponding_sentence)
+        all_texts.append(as_text)
+        print(all_texts)
     
-    # On va créer les expressions à comparer.
-    compare_strings = []
-    for n_gram in range(1, max_MWE_length + 1):
-        for i in range(len(target_sent)):
-            compare_strings.append(" ".join(target_sent[i:i+n_gram]))
-    print(compare_strings)
+        for text in all_texts[1:]:
+            source = all_texts[0]
+            target = text
+            print("\n\n")
+            run_align(source, target)
 
-    # model_path = hf_hub_download(repo_id="facebook/fasttext-fr-vectors", filename="model.bin")
-    # model = fasttext.load_model(model_path)
-    
-    for begin_index, end_index in source_spans:
-        corresponding_expression = " ".join(source_sent[begin_index: end_index])
-        source_as_vectors = embed(corresponding_expression)
-        print("Source sentence:" + " ".join(corresponding_expression))
-        cosine_sims = []
-        
-        # On va cherche le terme le plus proche dans la cible à partir de la source
-        # Il faudrait peut être trouver un encadrement pour gagner en précision. 
-        for target_sent in compare_strings:
-            target_as_vectors = embed(target_sent)
-            cosine_sims.append(cosine_similarity(source_as_vectors, target_as_vectors))
-        print(cosine_sims)
-            
-    
+def text_from_tokens(sentence):
+    as_list = []
+    for element in sentence.xpath("descendant::node()[self::tei:w or self::tei:pc]", namespaces=namespaces):
+        as_list.append(element.text)
+    return " ".join(as_list)
 
 if __name__ == '__main__':
-    source_sent = "It is possible to install all these dependencies without Anaconda (or with a lightweight alternative)"
-    target_sent = "Il est possible d'obtenir toutes les librairies nécessaires sans installer Anaconda (ou en choisissant plutôt une alternative plus légère)"
-    collate(source_sent.split(), target_sent.split(), max_MWE_length=7, source_spans=[(3,8)])
+    namespaces = {"tei": "http://www.tei-c.org/ns/1.0"}
+    main()
